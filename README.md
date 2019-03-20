9aafbbf00964c78f39b70d57df5ca3a0e0868b93
Fredrik Leemann <fredrik@leemann.se>
2019-03-20 19:07:53 +0100

    Added some styling to the shop

---
+++ README.md
@@ -0,0 +1,1 @@
+## Simple webshop in - C#, html, css and javascript
--- freshop/wwwroot/assets/product.js
+++ freshop/wwwroot/assets/product.js
@@ -2,9 +2,21 @@ const pageUrl = document.URL;
 const queryString = pageUrl.split('=')[1].toUpperCase();
 
 const productImage = document.getElementById('product_image');
+const productDesc = document.getElementById('product_desc');
+const artnr = document.getElementById('artnr');
+const brand = document.getElementById('brand');
+const price = document.getElementById('price');
+const model = document.getElementById('model');
 
 fetch('http://localhost:63492/api/products/'+queryString)
     .then(response => response.json())
     .then(json => {
+        console.log(json);
         productImage.attributes.src.value='./assets/images/'+json[0].img
+        productDesc.textContent = json[0].desc
+        model.textContent = json[0].model
+
+        artnr.textContent = json[0].pn
+        brand.textContent = json[0].brand
+        price.textContent = json[0].price
     });
