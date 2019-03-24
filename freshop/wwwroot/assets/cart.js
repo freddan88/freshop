4c784b7a898f35b0b353167b5c37321b5e5bc46c
Fredrik Leemann <fredrik@leemann.se>
2019-03-24 01:08:44 +0100

    Worked on page for cartitems and displayed them using JS

--- freshop/wwwroot/assets/cart.js
+++ freshop/wwwroot/assets/cart.js
@@ -4,4 +4,24 @@ const cartItems = document.querySelector('.cart-items');
 
 fetch('http://localhost:63492/api/orders/' + queryString)
     .then(response => response.json())
-    .then(json => console.dir(json));
+    .then(json => {
+        console.dir(json)
+            const cart = json;
+            cart.forEach(item => {
+            
+let output = `<div class="cart-item">
+            <img src="./assets/images/${item.img}" alt="${item.model}">
+            
+            <ul>
+                <li><small>Art.Nr: ${item.pn}</small></li>
+                <li><small>Model: ${item.model}</small></li>
+                <li><small>QTY: ${item.quantity}</small></li>
+                <li><small>Price(each): $${item.price}</small></li>
+            </ul>
+
+            <button id="delete-cartItem" data-guid="${item.product_guid}" data-itemID="${item.id}">X</button>
+        </div>`
+
+                cartItems.innerHTML += output;
+            });
+        });
